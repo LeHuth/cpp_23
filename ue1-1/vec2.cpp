581836f5c6e85e67cef433f1a99e778aa4c30a2f
leo-denkbuch <leonard.huth@googlemail.com>
2023-04-13 10:47:00 +0200

    init for ue1-1

---
+++ ue1-1/vec2.cpp
@@ -0,0 +1,3 @@
+//
+// Created by leo on 13.04.23.
+//
---
+++ ue1-1/vec2.h
@@ -0,0 +1,8 @@
+//
+// Created by leo on 13.04.23.
+//
+
+#ifndef HELLO_WORLD_VEC2_H
+#define HELLO_WORLD_VEC2_H
+
+#endif //HELLO_WORLD_VEC2_H
